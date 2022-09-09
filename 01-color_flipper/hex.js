6c8572dd399e23d0296fbdc05c9252d3f6932efc
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-09-09 16:43:31 -0500

    Finished hex version - random hex color generated by choosing hex digits from an array using 6 random indexes

--- 01-color_flipper/hex.js
+++ 01-color_flipper/hex.js
@@ -1,1 +1,18 @@
-console.log("Hex.js");
+const hexDigits =
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("#btn");
+const bgColor = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let randomHexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = getRandomIndex(hexDigits);
+    randomHexColor = randomHexColor.concat(hexDigits[randomIndex]);
+  }
+  document.body.style.backgroundColor = randomHexColor;
+  bgColor.textContent = randomHexColor;
+});
+
+function getRandomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
