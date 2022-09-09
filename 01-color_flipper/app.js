0c6d15823cdc420aa1cb8227931876d29501fc82
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-09-09 16:20:47 -0500

    Finished simple version - random index to choose from a hardcoded array of bg colors

--- 01-color_flipper/app.js
+++ 01-color_flipper/app.js
@@ -1,1 +1,13 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "blue"];
+const btn = document.querySelector("#btn");
+const bgColor = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomIndex = getRandomIndex(colors);
+  document.body.style.backgroundColor = colors[randomIndex];
+  bgColor.textContent = colors[randomIndex];
+});
+
+function getRandomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
